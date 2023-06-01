098ca8c712f43f7187368a42c32da234efa5a364
MemoRodriguez28 <memorodriguez2809@outlook.com>
2023-05-31 20:17:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from mt import *
 
 #Inversión de bits (igual que en tabla)
 funcionTransicion = {("q0","0"):("1", "R", "q0"),
-                     ("q0","1"):("0", "R", "q0"),
-                     ("q0"," "):(" ", " ", "qfinal")}
+                     ("q0","1"):("0", "R", "q1"),
+                     ("q1","0"):("1", "R", "q1"),
+                     ("q1","1"):("0", "R", "qfinal")}
 qfinales = {"qfinal"}
 
-mt = MT("1010101001 ", q0="q0", qfinales=qfinales, funcionTransicion=funcionTransicion)
+mt = MT("0101 ", q0="q0", qfinales=qfinales, funcionTransicion=funcionTransicion)
 
 print("Input en Cinta: \n" + mt.getCinta())
 
